1700ef27a98fb43caa4ba9cf73c379f90995d6e5
Shehzad <shehzad.lokhandwalla@oneacrefund.org>
2017-03-15 15:36:13 +0300

    Changed manifest and add dummy functions

--- app/background.js
+++ app/background.js
@@ -3,7 +3,26 @@
 // found in the LICENSE file.
 
 // Called when the user clicks on the browser action.
+
+// Todo: Add logic here...
+
+
+// Sample code to see how the extension interacts with the browser
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   var action_url = "javascript:window.print();";
   chrome.tabs.update(tab.id, {url: action_url});
 });
+
+var function readPageHeader ()
+{
+
+}
+var function updateDictionary ()
+{
+
+}
+var function emojiScore ()
+{
+
+}
